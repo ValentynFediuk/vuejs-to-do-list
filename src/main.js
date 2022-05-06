1a4721de996290c2be1be023333f5e4f3e323315
Valentyn <42549484+ValentynFediuk@users.noreply.github.com>
2022-05-06 10:43:10 +0300

    Revert "[updated] firestore integration (errors)"

--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
-import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import router from "./router";
+import {createApp} from 'vue'
 
-createApp(App).use(router).mount('#app')
+const firebaseConfig = {
+    apiKey: "AIzaSyBB5i7SmgbhBrIWi-gp0bOs4pIqBG1MRsI",
+    authDomain: "vjtdl-vf.firebaseapp.com",
+    projectId: "vjtdl-vf",
+    storageBucket: "vjtdl-vf.appspot.com",
+    messagingSenderId: "321907261687",
+    appId: "1:321907261687:web:ee940e3ba15a84f49193f2"
+};
+
+
+
+initializeApp(firebaseConfig);
+
+const app = createApp(firebaseConfig)
+const db = getFirestore(app);
+
+console.log(db)
+
+app.use(router)
+
+app.mount('#app')
